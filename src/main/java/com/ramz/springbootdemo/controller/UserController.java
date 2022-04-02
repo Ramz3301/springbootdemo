c5ca62a79da0e1dab1386cbd8a2e7684713769b7
Ramzan Kurbanov <ramkurbanov10@yandex.ru>
2022-04-02 09:26:01 +0300

    add user create functionality

--- src/main/java/com/ramz/springbootdemo/controller/UserController.java
+++ src/main/java/com/ramz/springbootdemo/controller/UserController.java
@@ -2,20 +2,39 @@ package com.ramz.springbootdemo.controller;
 
 import com.ramz.springbootdemo.model.User;
 import com.ramz.springbootdemo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
 @Controller
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/users")
-    public String findAll() {
+    public String findAll(Model model) {
         List<User> users = userService.findAll();
+        model.addAttribute("users", users);
+        return "user-list";
+    }
 
-        return "";
+    @GetMapping("/user-create")
+    public String createUserForm(User user) {
+        return "user-create";
+    }
+
+    @PostMapping("user-create")
+    public String createUser(User user) {
+        userService.saveUser(user);
+        return "redirect:/users";
     }
 }
