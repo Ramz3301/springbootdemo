e6cf7a32401a7b106070b353665883ec838c0bb3
Ramzan Kurbanov <ramkurbanov10@yandex.ru>
2022-04-02 09:03:15 +0300

    Initial commit

---
+++ src/test/java/com/ramz/springbootdemo/SprinhbootdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramz.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinhbootdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
